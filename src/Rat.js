3adce1d7f5c214dab6fa4de4aa55b4011b9f4431
Justin Carruthers <jtcruthers@gmail.com>
2020-08-18 21:30:52 -0400

    Added setup

--- src/Rat.js
+++ src/Rat.js
@@ -1,17 +1,60 @@
-//import { INVALID_MOVE } from 'boardgame.io/core';
+const SUITS = ["H", "S", "D", "C"];
+const RANKS = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+
+const setup = (ctx) => {
+  const initDeck = (ctx) => {
+    let deck = [];
+    SUITS.forEach((suit) => {
+      RANKS.forEach((rank) => deck.push({ suit, rank }));
+    });
+    return ctx.random.Shuffle(deck);
+  };
+
+  const initPlayers = (ctx) => {
+    let players = [];
+    for (let i = 0; i < ctx.numPlayers; i++) {
+      players.push({
+        hand: [],
+        handsWon: 0,
+        isStillIn: true,
+      });
+    }
+    return players;
+  };
+
+  return {
+    deck: initDeck(ctx),
+    players: initPlayers(ctx),
+    cardsPlayed: [],
+    trumps: "H",
+    numberOfCards: 3,
+    hasPickedTrumps: true,
+    roundLeaderPos: 0,
+  };
+};
 
 export const Rat = {
-    setup: () => ({ x: 1}),
+  setup,
 
-    turn: {
-        moveLimit: 1,
-    },
+  turn: {
+    moveLimit: 1,
+  },
 
-    moves: {
+  moves: {},
 
-    },
-
-    endIf: () => {
-
-    }
+  endIf: () => {},
 };
